abfaeaf16d7c04f7ecbb15aa784530ba011935f8
shenxfs <380406785@qq.com>
2022-06-15 06:48:25 +0800

    release V2.0.0
    高度范围-2000～25000米，格式化代码，加入Doxygen注释，修正一些缺陷。
    版本V2.0.0

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,12 @@
+/**
+ * @brief 主窗口
+ * @file mainwindow.cpp
+ * @author shenxf (380406785@qq.com)
+ * @version V2.0.0
+ * @date 2022-06-15
+ *
+ * 主窗口类实现 高度与静压相互转换，高度范围-2000～25000米，通过串口接收数据并解析，检测数据包的间隔周期并显示，显示数据包的标志字节。
+ */
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include"settingsdialog.h"
@@ -7,6 +16,12 @@
 #include <QLabel>
 #include <QDebug>
 
+/**
+ * @brief MainWindow::MainWindow
+ * @param parent
+ *
+ * 主窗口类构造函数
+ */
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -28,17 +43,23 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(ui->actionAbout, SIGNAL(triggered()), this, SLOT(about()));
     connect(serial, SIGNAL(readyRead()), this, SLOT(readData()));
     received = false;
-    echodata =false;
-    cycle =0;
+    echodata = false;
+    cycle = 0;
     st.tv_sec = 0;
     st.tv_usec = 0;
     ed.tv_sec = 0;
     ed.tv_usec = 0;
     frmSTA = 0U;
-    frmSize =0U;
+    frmSize = 0U;
     frmChksum = 0U;
-    showStatusMessage(tr("未连接"));   
- }
+    showStatusMessage(tr("未连接"));
+}
+
+/**
+ * @brief MainWindow::~MainWindow
+ *
+ * 析构函数
+ */
 MainWindow::~MainWindow()
 {
     delete ui;
@@ -46,73 +67,111 @@ MainWindow::~MainWindow()
     delete status;
 }
 
+/**
+ * @brief MainWindow::on_spinBox_valueChanged
+ * @param arg1 高度值
+ *
+ * 高度选择框内容改变回调函数，显示对应的静压值
+ */
 void MainWindow::on_spinBox_valueChanged(int arg1)
 {
     QString str;
-    str.sprintf("%7.3f",get_ps(arg1));
+    QTextStream s(&str);
+    s.setFieldWidth(7);
+    s.setFieldAlignment(QTextStream::AlignRight);
+    s.setRealNumberPrecision(3);
+    s << Qt::fixed << get_ps(arg1);
     ui->lineEdit->setText(str);
 }
 
+/**
+ * @brief MainWindow::on_lineEdit_textChanged
+ * @param arg1 静压值
+ *
+ * 静压编辑框内容改变回调函数，显示对应高度值
+ */
 void MainWindow::on_lineEdit_textChanged(const QString &arg1)
 {
-    double ps;
     QString str;
-    ps=arg1.toDouble();
-    ps = get_altu(ps);
-    str.sprintf("%5d",static_cast<int>(ps)) ;
+    QTextStream s(&str);
+    s.setFieldWidth(6);
+    s.setFieldAlignment(QTextStream::AlignRight);
+    s.setRealNumberPrecision(0);
+    s << Qt::fixed << get_altu(arg1.toDouble());
     ui->highEdit->setText(str);
 }
+
+/**
+ * @brief MainWindow::get_altu
+ * @param ps 静压值
+ * @return 高度值
+ *
+ * 静压转高度
+ */
 double MainWindow::get_altu(double ps)
 {
-   double high;
-   if(ps>PbN)
-   {
-       high =-2000;
-   }
-    else if(ps>Pb1)
-   {
-        high = Tb0/B*(qExp(-B*R/gn*qLn(ps/Pb0))-1.0);
-        if(high>0)
-        {
-            high += 0.5;
-        }
-        else
-        {
-            high -= 0.5;
-        }
-   }
-   else if (ps>Pb2)
-   {
-        high = 11000  +(- R*Tb1/gn*qLn(ps/Pb1)+0.5);
-   }
-   else
-   {
-       high = 20000;
-   }
-    return high;
-}
-double MainWindow::get_ps(int h)
-{
-    double ps;
-    if(h<-2000)
+    double high;
+    if(ps > PbN)
     {
-        ps = PbN;
+        high = -2000;
     }
-    else if(h<11000)
+    else if(ps > Pb1)
     {
-        ps = Pb0*qPow(((h*B)+Tb0)/Tb0,-gn/R/B);
+        high = Tb0 / B * (qExp(-B * R / gn * qLn(ps / Pb0)) - 1.0);
     }
-    else if(h<20000)
+    else if (ps > Pb2)
     {
-        ps = Pb1*qExp(-gn/Tb1/R*(h-11000));
+        high = 11000  + (- R * Tb1 / gn * qLn(ps / Pb1) );
+    }
+    else if(ps > Ps25km)
+    {
+        high = 20000 + Tb1 / B1 * (qExp(-B1 * R / gn * qLn(ps / Pb2)) - 1.0);
     }
     else
     {
-        ps  = Pb2;
+        high = 25000;
+    }
+    return high;
+}
+
+/**
+ * @brief MainWindow::get_ps
+ * @param h 高度值
+ * @return 静压值
+ *
+ * 高度转静压
+ */
+double MainWindow::get_ps(int h)
+{
+    double ps;
+    if(h < -2000)
+    {
+        ps = PbN;
+    }
+    else if(h < 11000)
+    {
+        ps = Pb0 * qPow(((h * B) + Tb0) / Tb0, -gn / R / B);
+    }
+    else if(h < 20000)
+    {
+        ps = Pb1 * qExp(-gn / Tb1 / R * (h - 11000));
+    }
+    else if(h < 25000)
+    {
+        ps = Pb2 * qPow((((h - 20000) * B1) + Tb1) / Tb1, -gn / R / B1);
+    }
+    else
+    {
+        ps  = Ps25km;
     }
     return ps;
 }
 
+/**
+ * @brief MainWindow::open_serial
+ *
+ * 打开按钮槽函数，打开串口
+ */
 void MainWindow::open_serial()
 {
     SettingsDialog::Settings p = settings->settings();
@@ -128,39 +187,49 @@ void MainWindow::open_serial()
         ui->actionDisconnect->setEnabled(true);
         ui->actionSetup->setEnabled(false);
         showStatusMessage(tr("已连接至 %1 : %2, %3, %4, %5, %6")
-              .arg(p.name).arg(p.stringBaudRate).arg(p.stringDataBits)
-              .arg(p.stringParity).arg(p.stringStopBits).arg(p.stringFlowControl));
+                          .arg(p.name).arg(p.stringBaudRate).arg(p.stringDataBits)
+                          .arg(p.stringParity).arg(p.stringStopBits).arg(p.stringFlowControl));
     }
     else
     {
         QMessageBox::critical(this, tr("错误"), serial->errorString());
-
         showStatusMessage(tr("串口打开错误"));
     }
     echodata = true;
     ui->textEdit->setText("");
 }
 
+/**
+ * @brief MainWindow::close_serial
+ *
+ * 关闭按钮槽函数，关闭串口
+ */
 void MainWindow::close_serial()
 {
     if (serial->isOpen())
+    {
         serial->close();
+    }
     ui->actionConnect->setEnabled(true);
     ui->actionDisconnect->setEnabled(false);
     ui->actionSetup->setEnabled(true);
     showStatusMessage(tr("已断开"));
     received = false;
     cycle = 0;
-    echodata =false;
+    echodata = false;
 }
 
+/**
+ * @brief MainWindow::about
+ *
+ * 显示版本信息
+ */
 void MainWindow::about()
 {
-    QMessageBox::about(this, tr("关于高度传感器显示程序V1.1.0"),
-                       tr("高度传感器显示程序基于Qt5.12.3开发，将海拔高度(m)转换为压力(kPa)并显示，"
+    QMessageBox::about(this, tr("关于高度传感器显示程序V2.0.0"),
+                       tr("高度传感器显示程序基于Qt5开发，将海拔高度(m)转换为压力(kPa)并显示，"
                           "通过接收高度传感器串行信号，显示高度传感器2路海拔高度结果，并与给定的海拔高度进行"
                           "比较作出是否符合要求的结论，检测高度传感器串行信号的更新率和完整性。"));
-
 }
 
 void MainWindow::showStatusMessage(const QString &message)
@@ -168,29 +237,33 @@ void MainWindow::showStatusMessage(const QString &message)
     status->setText(message);
 }
 
+/**
+ * @brief MainWindow::readData
+ *
+ * 串口接收回调函数 处理数据包信息
+ */
 void MainWindow::readData()
 {
     QByteArray data = serial->readAll();
-    quint8 i,ch;
-    for(i=0;i<data.size();i++)
+    quint8 i, ch;
+    for(i = 0; i < data.size(); i++)
     {
         ch = static_cast<quint8>(data.at(i));
-//        qDebug()<<hex<<"STA="<<frmSTA<<",DATA"<<ch;
-        if(0==frmSTA)
+        if(0 == frmSTA)
         {
-            if(0xAAU==ch)
+            if(0xAAU == ch)
             {
                 frmdata[0] = ch;
                 frmChksum += ch;
                 if(!received)
                 {
-                    gettimeofday(&st,nullptr);
+                    gettimeofday(&st, nullptr);
                     received = true;
                 }
                 else
                 {
-                    gettimeofday(&ed,nullptr);
-                    cycle =static_cast<qint32>(ed.tv_sec-st.tv_sec)*1000000L+(ed.tv_usec-st.tv_usec);
+                    gettimeofday(&ed, nullptr);
+                    cycle = static_cast<qint32>(ed.tv_sec - st.tv_sec) * 1000000L + (ed.tv_usec - st.tv_usec);
                     st.tv_sec = ed.tv_sec;
                     st.tv_usec = ed.tv_usec;
                 }
@@ -246,10 +319,10 @@ void MainWindow::readData()
         }
         else if(4U == frmSTA)
         {
-            frmdata[frmSize+4] = ch;
+            frmdata[frmSize + 4] = ch;
             frmChksum += ch;
             frmSize++;
-            if(frmSize>=6U)
+            if(frmSize >= 6U)
             {
                 frmSTA = 5U;
                 frmSize = 0;
@@ -261,30 +334,30 @@ void MainWindow::readData()
             if(0 == frmChksum)
             {
                 frmdata[10] = ch;
-                QString strOne="";
+                QString strOne = "";
                 QString str;
-                for(quint8 j=0;j<11;j++)
+                for(quint8 j = 0; j < 11; j++)
                 {
-                    str.sprintf("%02X ",static_cast<quint8>(frmdata[j]));
-                    strOne +=str;
+                    str = QString("%1 ").arg(static_cast<quint8>(frmdata[j]), 2, 16, QLatin1Char('0'));
+                    strOne += str;
                 }
                 ui->textEdit->append(strOne);
-                quint16 high=0;
-                high = static_cast<quint16>(frmdata[6])*256U+static_cast<quint8>(frmdata[5]);
-                str.sprintf("%5d",static_cast<qint16>(high));
+                quint16 high = 0;
+                high = static_cast<quint16>(frmdata[6]) * 256U + static_cast<quint8>(frmdata[5]);
+                str = QString("%1").arg(high, 5, 10);
                 ui->lineEdit_high1->setText(str);
-                high =static_cast<quint16>(frmdata[8])*256U+static_cast<quint8>(frmdata[7]);
-                str.sprintf("%5d",static_cast<qint16>(high));
+                high = static_cast<quint16>(frmdata[8]) * 256U + static_cast<quint8>(frmdata[7]);
+                str = QString("%1").arg(high, 5, 10);
                 ui->lineEdit_high2->setText(str);
                 if(echodata)
                 {
-                    str.sprintf("%02X",static_cast<quint8>(frmdata[4]));
+                    str = QString("%1").arg(static_cast<quint8>(frmdata[4]), 2, 16, QLatin1Char('0'));
                     ui->lineEdit_4->setText(str);
-                    str.sprintf(("%5.2fms"),cycle/1000.0);
+                    str = QString("%1").arg(cycle / 1000.0, 5, 'g', 3);
                     ui->lineEdit_5->setText(str);
                 }
             }
-            frmSTA =0;
+            frmSTA = 0;
             frmChksum = 0;
         }
         else
@@ -295,25 +368,36 @@ void MainWindow::readData()
     }
 }
 
-
-
-
+/**
+ * @brief MainWindow::on_lineEdit_high1_textChanged
+ * @param arg1 高度值
+ *
+ * 高度1编辑框内容改变回调函数，显示对应静压
+ */
 void MainWindow::on_lineEdit_high1_textChanged(const QString &arg1)
 {
-    double ps;
     QString str;
-    ps=arg1.toDouble();
-    ps = get_ps(static_cast<int>(ps));
-    str.sprintf("%7.3f",ps) ;
+    QTextStream s(&str);
+    s.setFieldWidth(7);
+    s.setFieldAlignment(QTextStream::AlignRight);
+    s.setRealNumberPrecision(3);
+    s << Qt::fixed << get_ps(arg1.toInt());
     ui->lineEdit_2->setText(str);
 }
 
+/**
+ * @brief MainWindow::on_lineEdit_high2_textChanged
+ * @param arg1 高度值
+ *
+ * 高度2编辑框内容改变回调函数，显示对应静压
+ */
 void MainWindow::on_lineEdit_high2_textChanged(const QString &arg1)
 {
-    double ps;
     QString str;
-    ps=arg1.toDouble();
-    ps = get_ps(static_cast<int>(ps));
-    str.sprintf("%7.3f",ps) ;
+    QTextStream s(&str);
+    s.setFieldWidth(7);
+    s.setFieldAlignment(QTextStream::AlignRight);
+    s.setRealNumberPrecision(3);
+    s << Qt::fixed << get_ps(arg1.toInt());
     ui->lineEdit_3->setText(str);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,3 +1,7 @@
+/**
+ * @brief 主窗口类头文件
+ *
+ */
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 #include<sys/time.h>
@@ -6,8 +10,9 @@
 #include <QtCore/QtGlobal>
 #include <QtCore/QQueue>
 #include <QTimer>
-namespace Ui {
-class MainWindow;
+namespace Ui
+{
+    class MainWindow;
 }
 
 class  SettingsDialog;
@@ -18,14 +23,14 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
     double get_altu(double ps);
     double get_ps(int h);
-    quint8 frmSTA;
-    quint8 frmSize;
-    quint8 frmChksum;
-    quint8 frmdata[11];
+    quint8 frmSTA;/**<接收数据包的状态机的状态标志*/
+    quint8 frmSize;/**<接收包的长度*/
+    quint8 frmChksum;/**<接收包的检验和*/
+    quint8 frmdata[11];/**<接收包的缓冲区*/
 private slots:
     void on_spinBox_valueChanged(int arg1);
     void on_lineEdit_textChanged(const QString &arg1);
@@ -39,22 +44,24 @@ private slots:
 private:
     Ui::MainWindow *ui;
     void showStatusMessage(const QString &message);
-    const double R = 287.05287;
-    const double gn = 9.80665;
-    const double Tb0= 288.15;
-    const double Pb0 =101.325;
-    const double PbN =127.773730122932510017142;
-    const double Tb1= 216.65;
-    const double Pb1 =22.632040095007798974347;
-    const double Pb2 = 5.47487742428104650521766;
-    const double B = -0.0065;
+    const double R = 287.05287;/**<气体常数*/
+    const double gn = 9.80665;/**<重力加速度*/
+    const double Tb0 = 288.15;/**<大气层下界（0～11000米）静温，开尔文*/
+    const double Pb0 = 101.325;/**<大气层下界（0～11000米）静压，kPa*/
+    const double PbN = 127.773730122932510017142;/**<大气层下界（-2000～0米）静压，kPa*/
+    const double Tb1 = 216.65;/**<大气层下界（11000～20000米）静温，开尔文*/
+    const double Pb1 = 22.632040095007798974347;/**<大气层下界（11000～20000米）静压，kPa*/
+    const double Pb2 = 5.47487742428104650521766;/**<大气层下界（20000～32000米）静压，kPa*/
+    const double Ps25km = 2.5110168179486166776514;/**<25000米对应的静压，kPa*/
+    const double B = -0.0065;/**<大气层（0～11000米）温度梯度，K/m*/
+    const double B1 = 0.001;/**<大气层（20000～32000米）温度梯度，K/m*/
     QSerialPort *serial;
-     SettingsDialog  *settings;
-     QLabel *status;
-     struct timeval st,ed;
-     bool received;
-     bool echodata;
-     qint32 cycle;
+    SettingsDialog  *settings;
+    QLabel *status;
+    struct timeval st, ed;
+    bool received;
+    bool echodata;
+    qint32 cycle;
 };
 
 #endif // MAINWINDOW_H
